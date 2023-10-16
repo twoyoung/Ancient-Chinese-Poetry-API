4aa5af539b90597f61f8950ac0e1aaa88113426f
twoyoung <yyoung1025@gmail.com>
2023-10-17 02:55:12 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env'});
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB).then(() => console.log('Connected to the database')).catch(err => console.log(err));
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'ChinesePoetry' // Specify your desired database name here
+  }).then(() => console.log('Connected to the database')).catch(err => console.log(err));
 
-const app = express();
 
 const port = process.env.PORT || 3000;
-
-
 
 
 server = app.listen(port, () => {
