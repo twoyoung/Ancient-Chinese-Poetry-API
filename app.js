dd8368a10629646a3798eaf480877d069f2c19e2
twoyoung <yyoung1025@gmail.com>
2023-10-18 19:31:03 +1300

    add rate limit function

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const Poetry = require("./models/poetryModel");
 const song = require("./models/songModel");
+const rateLimit = require("express-rate-limit");
 const poetryController = require("./controllers/poetryController");
 
 const app = express();
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again an hour later."
+})
+
+app.use('/', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
